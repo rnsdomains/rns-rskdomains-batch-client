d64e707ed0233c650cb12654049646713f7d6d40
Ilan Olkies <ilanolkies@outlook.com>
2020-03-10 18:59:26 -0300

    Skeleton

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,36 @@
-export default () => '.rsk domains batch';
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+export default () => (
+  <Container style={{ textAlign: 'center' }}>
+    <Row>
+      <Col>
+        <h1>.rsk domains batch</h1>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <h3>Choose domains</h3>
+      </Col>
+    </Row>
+    <Row><Col><hr /></Col></Row>
+    <Row>
+      <Col>
+        <h3>Validate domains</h3>
+      </Col>
+    </Row>
+    <Row><Col><hr /></Col></Row>
+    <Row>
+      <Col>
+        <h3>Calculate price</h3>
+      </Col>
+    </Row>
+    <Row><Col><hr /></Col></Row>
+    <Row>
+      <Col>
+        <h3>Execute registration</h3>
+      </Col>
+    </Row>
+  </Container>
+)
+
