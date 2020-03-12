7822ff5cc8f48cecbf998ee4a24b92b175a1074c
Ilan Olkies <ilanolkies@outlook.com>
2020-03-12 06:05:14 -0300

    Updates

---
+++ precache-manifest.564993f773ff918527d0953a16a22cc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009dc5d90bfff844edda86bef1c8db6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "908ff70ade2ad9d2fb26",
+    "url": "/static/js/2.b19477a7.chunk.js"
+  },
+  {
+    "revision": "d3001871c80f70868d81db5350068385",
+    "url": "/static/js/2.b19477a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ebe861259977e690416",
+    "url": "/static/js/main.2121d256.chunk.js"
+  },
+  {
+    "revision": "d1fedcc0cbc9aa39cbd1",
+    "url": "/static/js/runtime-main.4c8b26bd.js"
+  }
+]);
